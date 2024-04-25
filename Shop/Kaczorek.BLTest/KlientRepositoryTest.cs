d4d90586c1f7da5b19487d9535d3ae6b2dceba18
matrixxx602 <159135973+Matrixxx602@users.noreply.github.com>
2024-04-25 19:58:44 +0200

    Dodanie aplikacji z kursu.
    Utworzenie projektu z logiką biznesową oraz testami.

---
+++ Shop/Kaczorek.BLTest/KlientRepositoryTest.cs
@@ -0,0 +1,87 @@
+﻿using Kaczorek.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Kaczorek.BLTest
+{
+    [TestClass]
+    public class KlientRepositoryTest
+    {
+        [TestMethod]
+        public void PobierzKlientaTest()
+        {
+            // Arrange (Przygotuj test)
+            var klientRepository = new KlientRepository();
+            var oczekiwana = new Klient(1)
+            {
+                Email = "malpa@o2.pl",
+                Imie = "Julian",
+                Nazwisko = "Tusk"
+
+            };
+
+            // Act (Działaj)
+            var aktualna = klientRepository.Pobierz(1);
+
+
+            // Assert (Potwierdz test)
+            //Assert.AreEqual(oczekiwana, aktualna);
+            Assert.AreEqual(oczekiwana.KlientID, aktualna.KlientID);
+            Assert.AreEqual(oczekiwana.Email, aktualna.Email);
+            Assert.AreEqual(oczekiwana.Imie, aktualna.Imie);
+            Assert.AreEqual(oczekiwana.Nazwisko, aktualna.Nazwisko);
+
+        }
+        [TestMethod]
+        public void PobierzKlientazAdresamiTest()
+        {
+            // Arrange (Przygotuj test)
+            var klientRepository = new KlientRepository();
+            var oczekiwana = new Klient(1)
+            {
+                Email = "malpa@o2.pl",
+                Imie = "Julian",
+                Nazwisko = "Tusk",
+                ListaAdresow = new List<Adres>()
+                {
+                    new Adres()
+                    {
+                        AdresTyp = 1,
+                        Ulica = "Krótka",
+                        Miasto = "Lublin",
+                        Kraj = "Polska",
+                        KodPocztowy = "11-112"
+                    },
+
+                    new Adres()
+                    {
+                        AdresTyp = 3,
+                        Ulica = "Nowa",
+                        Miasto = "Krakow",
+                        Kraj = "Polska",
+                        KodPocztowy = "13-112"
+                    }
+                }
+            };
+
+            // Act (Działaj)
+            var aktualna = klientRepository.Pobierz(1);
+
+            // Assert (Potwierdz test)
+            //Assert.AreEqual(oczekiwana, aktualna);
+            Assert.AreEqual(oczekiwana.Email, aktualna.Email);
+            Assert.AreEqual(oczekiwana.Imie, aktualna.Imie);
+            Assert.AreEqual(oczekiwana.Nazwisko, aktualna.Nazwisko);
+
+            for (int i = 0; i < 1; i++)
+            {
+                Assert.AreEqual(oczekiwana.ListaAdresow[i].AdresTyp, aktualna.ListaAdresow[i].AdresTyp);
+                Assert.AreEqual(oczekiwana.ListaAdresow[i].Ulica, aktualna.ListaAdresow[i].Ulica);
+                Assert.AreEqual(oczekiwana.ListaAdresow[i].Miasto, aktualna.ListaAdresow[i].Miasto);
+                Assert.AreEqual(oczekiwana.ListaAdresow[i].Kraj, aktualna.ListaAdresow[i].Kraj);
+                Assert.AreEqual(oczekiwana.ListaAdresow[i].KodPocztowy, aktualna.ListaAdresow[i].KodPocztowy);
+            }
+        }
+    }
+}
---
+++ Shop/Kaczorek.BLTest/KlientTest.cs
@@ -0,0 +1,116 @@
+﻿using Kaczorek.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Kaczorek.BLTest
+{
+    [TestClass]
+    public class KlientTest
+    {
+        [TestMethod]
+        public void ImieNazwiskoTest()
+        {
+            // Arrange (Przygotuj test)
+            Klient klient = new Klient();
+            klient.Imie = "Skieta";
+            klient.Nazwisko = "Tusk";
+
+            string oczekiwana = "Skieta Tusk";
+
+            // Act (Działaj)
+            string aktualna = klient.ImieNazwisko;
+
+            // Assert (Potwierdz test)
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+
+        [TestMethod]
+        public void ImiePusteNazwiskoTest()
+        {
+            // Arrange (Przygotuj test)
+            Klient klient = new Klient();
+            klient.Imie = "";
+            klient.Nazwisko = "Tusk";
+
+            string oczekiwana = "Tusk";
+
+            // Act (Działaj)
+            string aktualna = klient.ImieNazwisko;
+
+            // Assert (Potwierdz test)
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+
+        [TestMethod]
+        public void ImieNazwiskoPusteTest()
+        {
+            // Arrange (Przygotuj test)
+            Klient klient = new Klient();
+            klient.Imie = "Skieta";
+            klient.Nazwisko = "";
+
+            string oczekiwana = "Skieta";
+
+            // Act (Działaj)
+            string aktualna = klient.ImieNazwisko;
+
+            // Assert (Potwierdz test)
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            // Arrange (Przygotuj test)
+            var kln1 = new Klient();
+            kln1.Imie = "Skieta";
+            Klient.Licznik += 1;
+
+            var kln2 = new Klient();
+            kln2.Imie = "Rosita";
+            Klient.Licznik += 1;
+
+            var kln3 = new Klient();
+            kln3.Imie = "Lori";
+            Klient.Licznik += 1;
+
+            // Act (Działaj)
+
+            // Assert (Potwierdz test)
+            Assert.AreEqual(3, Klient.Licznik);
+
+        }
+
+        [TestMethod]
+        public void ZwalidujTest()
+        {
+            // Arrange (Przygotuj test)
+
+            var klient1 = new Klient();
+            klient1.Nazwisko = "Skieta";
+            klient1.Email = "skieta@o2.pl";
+
+            var oczekiwana = true;
+
+            // Act (Działaj)
+
+            var aktualna = klient1.Zwaliduj();
+
+            // Assert (Potwierdz test)
+
+            Assert.AreEqual(oczekiwana, aktualna);
+
+        }
+
+        [TestMethod]
+        public void xxx()
+        {
+            // Arrange (Przygotuj test)
+            
+            // Act (Działaj)
+
+            // Assert (Potwierdz test)
+
+        }
+    }
+}
