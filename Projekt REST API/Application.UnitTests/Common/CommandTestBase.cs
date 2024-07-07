0c8a5eecefbf99eb4d770a9e683a19f9f41664ba
matrixxx602 <159135973+Matrixxx602@users.noreply.github.com>
2024-07-07 12:27:06 +0200

    Mockowanie bazy danych.
    Skonfigurowanie mokowalnej bazy danych z przykładowymi danymi.

--- Projekt REST API/Application.UnitTests/Common/CommandTestBase.cs
+++ Projekt REST API/Application.UnitTests/Common/CommandTestBase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Moq;
+using MovieManagement.Persistance;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace Application.UnitTests.Common
 {
-    public class CommandTestBase
+    public class CommandTestBase : IDisposable
     {
+        protected readonly MovieDbContext _context;
+        protected readonly Mock<MovieDbContext> _contextMock;
+        public CommandTestBase()
+        {
+            _contextMock = MovieDbContextFactory.Create();
+            _context = _contextMock.Object;
+        }
+        public void Dispose()
+        {
+            MovieDbContextFactory.Destroy(_context);
+        }
     }
 }
--- Projekt REST API/MovieManagement/obj/Debug/net5.0/MovieManagement.Api.AssemblyInfo.cs
+++ Projekt REST API/MovieManagement/obj/Debug/net5.0/MovieManagement.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3d4b5741ec3c3accc3f59925b26ac0a8ea3e45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661bd57a3188f5085be12531a152596082c88797")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
